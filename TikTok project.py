92765a19eabe40b20833d4290a8f7fb78fa01959
Alejandro Farinas <134167108+Thefez18-23@users.noreply.github.com>
2024-02-29 19:54:16 -0500

    Update TikTok project.py

--- TikTok project.py
+++ TikTok project.py
@@ -2,29 +2,25 @@
 # coding: utf-8
 
 # # **TikTok Project**
-# **Course 6 - The nuts and bolts of machine learning**
+# - The nuts and bolts of machine learning**
 
-# Recall that you are a data professional at TikTok. Your
+# You are a data professional at TikTok. Your
 # supervisor was impressed with the work you have done
 # and has requested that you build a machine learning model that can be used to determine whether a video contains a claim or whether it offers an opinion. With a successful prediction model, TikTok can reduce the backlog of user reports and prioritize them more efficiently.
 # 
 # A notebook was structured and prepared to help you in this project. Please complete the following questions.
 
-# # **Course 6 End-of-course project: Classifying videos using machine learning**
-# 
-# In this activity, you will practice using machine learning techniques to predict on a binary outcome variable.
-# <br/>
+ 
+#
 # 
 # **The purpose** of this model is to mitigate misinformation in videos on the TikTok platform.
 # 
 # **The goal** of this model is to predict whether a TikTok video presents a "claim" or presents an "opinion".
-# <br/>
-# 
-# *This activity has three parts:*
+
+
 # 
 # **Part 1:** Ethical considerations
-# * Consider the ethical implications of the request
-# 
+ 
 # * Should the objective of the model be adjusted?
 # 
 # **Part 2:** Feature engineering
